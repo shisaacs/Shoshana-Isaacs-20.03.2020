3a4eb18d80af38a711a4b4d93a47aed25ae4e296
Shoshana <isaacs.shoshana@gmail.com>
2020-03-27 15:10:44 +0300

    Updates

---
+++ precache-manifest.2ac5f5d202d04ca3e8fac0cfa00bb482.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "042f2a979b00a19c3dd1c05fc12e679e",
+    "url": "/Shoshana-Isaacs-20.03.2020/index.html"
+  },
+  {
+    "revision": "44f558da789ad962f6d8",
+    "url": "/Shoshana-Isaacs-20.03.2020/static/css/2.dec3253d.chunk.css"
+  },
+  {
+    "revision": "a8296631659934390a84",
+    "url": "/Shoshana-Isaacs-20.03.2020/static/css/main.75b13ee4.chunk.css"
+  },
+  {
+    "revision": "44f558da789ad962f6d8",
+    "url": "/Shoshana-Isaacs-20.03.2020/static/js/2.3d6c9979.chunk.js"
+  },
+  {
+    "revision": "d03bd439e12b1876420276a47980eb2f",
+    "url": "/Shoshana-Isaacs-20.03.2020/static/js/2.3d6c9979.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8296631659934390a84",
+    "url": "/Shoshana-Isaacs-20.03.2020/static/js/main.2d371bbd.chunk.js"
+  },
+  {
+    "revision": "c67ab8e4cba1f6abd3b3",
+    "url": "/Shoshana-Isaacs-20.03.2020/static/js/runtime-main.5d9aab3a.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/Shoshana-Isaacs-20.03.2020/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/Shoshana-Isaacs-20.03.2020/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/Shoshana-Isaacs-20.03.2020/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/Shoshana-Isaacs-20.03.2020/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/Shoshana-Isaacs-20.03.2020/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/Shoshana-Isaacs-20.03.2020/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/Shoshana-Isaacs-20.03.2020/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/Shoshana-Isaacs-20.03.2020/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/Shoshana-Isaacs-20.03.2020/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/Shoshana-Isaacs-20.03.2020/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/Shoshana-Isaacs-20.03.2020/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/Shoshana-Isaacs-20.03.2020/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/Shoshana-Isaacs-20.03.2020/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/Shoshana-Isaacs-20.03.2020/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/Shoshana-Isaacs-20.03.2020/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/Shoshana-Isaacs-20.03.2020/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
